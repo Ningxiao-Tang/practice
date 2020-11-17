3d8d615cca31ba41e340dcc8ecab63a9467ac2fc
Ningxiao-Tang <ellenx1128@gmail.com>
2020-11-17 14:42:50 -0500

    longest palindrome substring, dp solution

--- src/LongestPalindromeSubseq.java
+++ src/LongestPalindromeSubseq.java
@@ -1,5 +1,5 @@
 public class LongestPalindromeSubseq {
     public int longestPalindromeSubseq(String s) {
-
+        return 0;
     }
 }
--- src/LongestPalindromeSubstr.java
+++ src/LongestPalindromeSubstr.java
@@ -1,4 +1,5 @@
 public class LongestPalindromeSubstr {
+    //expand from center solution
     public String longestPalindrome(String s) {
         int best_len = 0;
         String best_s = "";
@@ -27,5 +28,36 @@ public class LongestPalindromeSubstr {
             }
         }
         return best_s;
+    }
+
+    //dynamic programming solution, a lot slower than the above solution
+    public String longestPalindrome2(String s) {
+        if (s == null || s.equals(""))
+            return "";
+        int n = s.length();
+        boolean[][] isPalindrome = new boolean[n][n];
+        int best_len = 1, start = 0;
+        // each character itself is a palindrome
+        for (int i = 0; i < n; i++)
+            isPalindrome[i][i] = true;
+
+        for (int i = 0; i < n - 1; i++) {
+            isPalindrome[i][i+1] = s.charAt(i) == s.charAt(i+1);
+            if (isPalindrome[i][i+1]) {
+                best_len = 2;
+                start = i;
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                isPalindrome[i][j] = s.charAt(i) == s.charAt(j) && isPalindrome[i+1][j-1];
+                if (isPalindrome[i][j] && j - i + 1 > best_len) {
+                    best_len = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start, start+best_len);
     }
 }
