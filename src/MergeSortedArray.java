8b29d626e8eba7b852eda543b855cbb40fc2fe50
Ningxiao-Tang <ellenx1128@gmail.com>
2020-11-24 21:26:15 -0500

    sort

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -1,10 +1,8 @@
 public class MergeSortedArray {
+    //O(nlogn)
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = 0, j = 0, k = 0;
-
-
     }
-
     public static int[] mergeSortedArray(int[] A, int[] B) {
         // write your code here
         int[] ans = new int[A.length + B.length];
